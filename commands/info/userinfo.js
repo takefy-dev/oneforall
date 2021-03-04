17ba78084fc756495fcf35a453019e85eae69451
baby <63604401+whoisbaby@users.noreply.github.com>
2021-03-04 15:27:57 +0200

    Update userinfo.js
    test

--- commands/info/userinfo.js
+++ commands/info/userinfo.js
@@ -51,18 +51,18 @@ module.exports = new Command({
 
     const flags = {
         DISCORD_EMPLOYEE: 'Discord Employee',
-        DISCORD_PARTNER: 'Discord Partner',
-        BUGHUNTER_LEVEL_1: 'Bug Hunter (Level 1)',
-        BUGHUNTER_LEVEL_2: 'Bug Hunter (Level 2)',
-        HYPESQUAD_EVENTS: 'HypeSquad Events',
-        HOUSE_BRAVERY: 'House of Bravery',
-        HOUSE_BRILLIANCE: 'House of Brilliance',
-        HOUSE_BALANCE: 'House of Balance',
-        EARLY_SUPPORTER: 'Early Supporter',
-        TEAM_USER: 'Team User',
-        SYSTEM: 'System',
-        VERIFIED_BOT: 'Verified Bot',
-        VERIFIED_DEVELOPER: 'Verified Bot Developer'
+        DISCORD_PARTNER: '<:Z_Partenaire2:816749957771427861>',
+        BUGHUNTER_LEVEL_1: '<:XDM_BugHunter:816750123996282882>',
+        BUGHUNTER_LEVEL_2: '<:level2bug:817023897378488350>',
+        HYPESQUAD_EVENTS: '<:YLM_Hypesquad:816750048838418462>',
+        HOUSE_BRAVERY: '<:bravery:817024570166214748>',
+        HOUSE_BRILLIANCE: '<:briliance:817024700273786900>',
+        HOUSE_BALANCE: '<:balance:817024823330996225>',
+        EARLY_SUPPORTER: '<:earlysupporter:783422670107836446>',
+        TEAM_USER: '<:XDM_Staff:816750074750828545>',
+        SYSTEM: '<:YLM_Shield:816750007776575499>',
+        VERIFIED_BOT: '<:verifiedbot:783422173003776030>',
+        VERIFIED_DEVELOPER: '<:dev:783422756854300713>'
     };
     const rolesGuild = message.guild.roles.cache.sort((a, b) => b.position - a.position).map(role => role.toString());
     const membersGuild = message.guild.members.cache;
@@ -72,6 +72,12 @@ module.exports = new Command({
     const color = guildEmbedColor.get(message.guild.id)
     
     const member = message.mentions.members.first() || message.guild.members.cache.get(argument) || message.member;
+var Statusfilter = {
+  online: `<:online2:801064570277724170>`,
+  idle: `<:charliewave_idle:786331151144714291>`,
+  dnd: `<:charliewave_dnd:786331160744689704>`,
+  offline: `<:charliewave_offline:786331156010106890>`
+};
     if(member.user.presence.status == "offline"){
         var online = "online"
     }else{
@@ -82,11 +88,24 @@ module.exports = new Command({
         .map(role => role.toString())
         .slice(0, -1);
     const userFlags = member.user.flags.toArray();
+  let checkbot = " ";
+
+  if (member.bot) checkbot = "Robot";
+  else checkbot = "Human";
     const embedUser = new Discord.MessageEmbed()
         .setThumbnail(member.user.displayAvatarURL({ dynamic: true, size: 512 }))
         .setColor(color)
-        .setTitle(`${member.user.username}`)
-        .addField('<a:fleche:786340501531262977> **USERINFO:**', [
+        .setTitle(`${member.user.username}#${member.user.discriminator}`)
+        .setDescription(`UID: ${member.id}`)
+        .addField(`**TAG:**`, `#${member.user.discriminator}`, true)
+        .addField(`**NICKNAME:**`, `${member.nickname ? member.nickname : "N/A"}`, true)
+        .addField(`**STATUS:**`, Statusfilter[member.presence.status], true)
+        .addField(`**HIGHEST ROLE:**`, `${member.roles.hoist ? member.roles.hoist.name : 'None'}`, true)
+        .addField(`**BADGES:**`, `${userFlags.length ? userFlags.map(flag => flags[flag]).join(', ') : 'None'}`, true)
+        .addField(`**TYPE:**`, `${checkbot}`, true)
+        .addField(`**CREATED:**`, `${moment(member.user.createdTimestamp).format('LT')} ${moment(member.user.createdTimestamp).format('LL')} ${moment(member.user.createdTimestamp).fromNow()}`, true)
+        .addField(`**JOINED:**`, `${moment(member.joinedAt).format('LL LTS')}`, true)
+        /*.addField('<a:fleche:786340501531262977> **USERINFO:**', [
             `Discriminatoir: **${member.user.discriminator}**`,
             `ID: **${member.id}**`,
             `Badges: **${userFlags.length ? userFlags.map(flag => flags[flag]).join(', ') : 'None'}**`,
@@ -103,7 +122,7 @@ module.exports = new Command({
             `Hoist Role: **${member.roles.hoist ? member.roles.hoist.name : 'None'}**`,
             `Roles [**${roles.length}**]: **${roles.length < 10 ? roles.join(', ') : roles.length > 10 ? this.client.utils.trimArray(roles) : 'None'}**`,
             `\u200b`
-        ]);
+        ]);*/
     return message.channel.send(embedUser);
 });
 
