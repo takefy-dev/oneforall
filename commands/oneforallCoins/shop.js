8b02d57c2ca5c7717cc2c2ce71b98fc85c8ec0c5
takefy-dev <jules001dixon@gmail.com>
2021-03-06 13:39:55 +0100

    made change to lang translate + shop and more simple check owner

---
+++ commands/oneforallCoins/shop.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js')
+const guildEmbedColor = new Map();
+const StateManager = require('../../utils/StateManager');
+var embedsColor = require('../../function/embedsColor');
+const {Command} = require('advanced-command-handler');
+const guildLang = new Map();
+var langF = require('../../function/lang')
+
+module.exports = new Command({
+    name: 'shop',
+    description: 'Show the shop / Add or remove item to the shop | Afficher le magasin / Ajouter ou supprimer des objet dans le shop',
+    // Optionnals :
+    usage: '!shop [add/remove]',
+    category: 'coins',
+    tags: ['guildOnly'],
+    aliases: ['store', 'magasin'],
+    clientPermissions: ['EMBED_LINKS', 'ADD_REACTIONS'],
+    cooldown: 3
+}, async(client, message, args) => {
+    const color = guildEmbedColor.get(message.guild.id);
+    const lang = require(`../../lang/${guildLang.get(message.guild.id)}`);
+    if(args[0] === 'add'){
+        client.isGuildOwner(message.guild.id, message.author.id)
+    }
+});
+
+embedsColor(guildEmbedColor);
+langF(guildLang);
