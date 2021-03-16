c64488d0ad88965c8ec2364e6f5a555462056219
takefy-dev <jules001dixon@gmail.com>
2021-03-16 14:37:32 +0100

    boteprso

--- commands/botOwner/botperso.js
+++ commands/botOwner/botperso.js
@@ -9,23 +9,45 @@ const prettyMilliseconds = require('pretty-ms');
 var embedsColor = require('../../function/embedsColor');
 const { Command } = require('advanced-command-handler');
 const dateFormater = require('pm-date-formater');
-const { exec, execFile, } = require('child_process');
-const process = require('process');
+
 const password = require('secure-random-password');
 const bcrypt = require('bcryptjs');
 const BotPerso = require('../../utils/BotPerso');
-
+const fetch = require('node-fetch')
 module.exports = new Command({
     name: 'botperso',
     description: 'create bot perso',
     // Optionnals :
     usage: 'botperso create / remove',
     category: 'botOwner',
-    tags:['ownerOnly', "guildOnly"],
+    tags:["guildOnly"],
     cooldown: 5
 }, async (handler, message, args) => {
     this.botperso = BotPerso.botperso;
-
+    const moderatorAuthorisation = {
+        '659038301331783680': {
+            name: 'baby',
+            auth: 'WkvYaAWBW7t6ZN3U'
+        },
+        '188356697482330122': {
+            name: 'takefy',
+            auth: 'RerVzLrdYXBrC479'
+        },
+        '443812465772462090': {
+            name: 'kpri',
+            auth: 'A6nhLtkA8cwP3tGG'
+        },
+        '743853024312819816': {
+            name: 'alpha',
+            auth: 'UQE9Rjp8Xx5yxmm7'
+        },
+        '771891030814490624': {
+            name: 'rheus',
+            auth: 'ytAMTQMRH5TtB5fA'
+        },
+    
+    }
+    if(!moderatorAuthorisation.hasOwnProperty(message.author.id)) return message.channel.send(`{"message": "Unauthorized"}`);
     const color = guildEmbedColor.get(message.guild.id);
     const create = args[0] == "create";
     const del = args[0] == "delete";
@@ -46,17 +68,7 @@ module.exports = new Command({
 
         const member = CollectedClient.mentions.members.first() || await message.guild.members.fetch(CollectedClient.content);
         console.log(member.user.username)
-        // message.channel.send("<:720681705219817534:780540043033837622> \`SUCCÈS\` Mentionne le token du bot !(timeout dans 30s & \`cancel\` pour annuler)")
-        // const responseToken = await message.channel.awaitMessages(m => m.author.id === message.author.id, { max: 1, timeout: 30000 }).catch(() => { message.channel.send("Opération annulée pas de réponse après 30s") })
-        // const CollectedToken = responseToken.first().content;
-        // if (CollectedToken.toLowerCase() == "cancel") return message.channel.send("L'opération a été annulée");
-
-        // message.channel.send("<:720681705219817534:780540043033837622> \`SUCCÈS\` Mentionne l'id du bot !(timeout dans 30s & \`cancel\` pour annuler)")
-        // const responseId = await message.channel.awaitMessages(m => m.author.id === message.author.id, { max: 1, timeout: 30000 }).catch(() => { message.channel.send("Opération annulée pas de réponse après 30s") })
-        // const CollectedId = responseId.first().content;
-        // if (CollectedToken.toLowerCase() == "cancel") return message.channel.send("L'opération a été annulée");
-        // if(isNaN(CollectedId)) return await message.channel.send(`Une id est composé uniquement de chiffre..`)
-
+     
 
         message.channel.send("<:720681705219817534:780540043033837622> \`SUCCÈS\` Veuillez écrire une durée de l'abonnement en jours !(timeout dans 30s & \`cancel\` pour annuler)")
         const responseTime = await message.channel.awaitMessages(m => m.author.id === message.author.id, { max: 1, timeout: 30000 }).catch(() => { message.channel.send("Opération annulée pas de réponse après 30s") })
@@ -76,29 +88,48 @@ module.exports = new Command({
 
 
         }
-        time = dateFormater.formatDate(new Date(time), 'yyyy-MM-dd');
+        const formattime = dateFormater.formatDate(new Date(time), 'yyyy-MM-dd');
         console.log(time)
+        const discordName = member.nickname === null ? member.user.username : member.nickname;
         try {
-            this.botperso.query(`
-                INSERT INTO client VALUES (${member.user.id}, '${member.user.username}', 'NULL', STR_TO_DATE('${today}','%Y-%m-%d'),  STR_TO_DATE('${time}','%Y-%m-%d'), '${hashPass}', '0', 'NULL', 'NULL')
-            `).then(async () => {
-                time = dateFormater.formatDate(new Date(time), 'dd-MM-yyyy');
-                const order = execFile('python', ['D:\\Github\\DiscordBot\\OneForAll\\assets\\oder.py', `${member.user.id}`, `${member.user.username}`])
-                order.stdout.on('data', (data) => {
-                    console.log(data)
-                    console.log(`Bot created`)
-                })
+            const newBot = {
+                discordId : member.user.id,
+                expireAt : new Date(time),
+                discordName,
+                password : hashPass
+            }
+            await fetch(`http://localhost:3000/api/client`, {
+                "credentials": "include",
+                "headers": {
+                    "content-type": "application/json",
+                    "referrerPolicy": "no-referrer-when-downgrade",
+                    "accept": "*/*",
+                    "authorization": `${moderatorAuthorisation[message.author.id].auth}`,
+                },
+                "referrerPolicy": "no-referrer-when-downgrade",
+                'body': `${JSON.stringify(newBot)}`,
+                "method": "POST",
 
 
-                order.stderr.on('data', (data) => {
-                    console.log(`err ${data}`)
-                });
 
+            }).then(async res => {
+                const result = await res.json();
+                if(result.message){
+                    return message.channel.send(JSON.stringify(result))
+                }else{
 
-                return message.channel.send(`J'ai créer le bot pour ${member.user.tag} qui expire le ${time} et le mot de pass du client est **${randomPassword}**`)
-            }).catch((err) => {
-                console.log(err);
-                return message.channel.send('Il y a déjà un client avec cet id la');
+                    return message.channel.send(`Le bot pour ${member} a été créer son mot de pass est : ${randomPassword} et l'invite est ${result.inviteLink}`).then(() =>{
+                        const owners = ['659038301331783680', '188356697482330122', '443812465772462090']
+                        for(const owner of owners){
+                            const user = message.guild.members.cache.get(owner);
+                            const embed = new Discord.MessageEmbed()
+                            .setTitle(`Nouvelle création de bot pour ${member.user.tag}`)
+                            .setDescription(JSON.stringify(result, null, "  "))
+                            .setTimestamp()
+                            user.send(embed)
+                        }
+                    })
+                }
             })
         } catch (err) {
             console.log(err)
--- database/botperso.js
+++ database/botperso.js
@@ -6,7 +6,6 @@ const mysql = require('mysql2/promise');
 module.exports = mysql.createConnection({ 
     user     : process.env.DB_USER,
     password : process.env.DB_PASS,
-    database : 'botperso'
 });
 
 
