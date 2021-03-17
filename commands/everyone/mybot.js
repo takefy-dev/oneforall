11d6b8e0ea00acbf1e017c17397496361606372c
takefy-dev <jules001dixon@gmail.com>
2021-03-17 13:02:01 +0100

    mybot

--- commands/everyone/mybot.js
+++ commands/everyone/mybot.js
@@ -4,13 +4,13 @@ const Discord = require('discord.js')
 const prettyMilliseconds = require('pretty-ms');
 const guildEmbedColor = new Map();
 var embedsColor = require('../../function/embedsColor');
+const fetch = require('node-fetch')
 const guildLang = new Map();
 var langF = require('../../function/lang')
 module.exports = new Command(
     {
         name: 'mybot',
-        description : `Show your current subscription of custom bot | Montre votre abonnement de bot personnalisé`,
-        usage: '!mybot',
+
         category: 'botperso',
         tags: ['guildOnly'],
         aliases: ['mybots', 'mesbot'],
@@ -20,28 +20,78 @@ module.exports = new Command(
     async (client, message, args) => {
         const color = guildEmbedColor.get(message.guild.id);
         const lang = require(`../../lang/${guildLang.get(message.guild.id)}`);
-        this.botperso = BotPerso.botperso;
-        this.botperso.query(`SELECT * FROM client WHERE discordId = '${message.author.id}'`).then(async (res) => {
-            if (res[0].length === 0) return message.channel.send(`Vous n'avez pas de bot personnalisé pour en commander un veuillez créer un ticket !`).then((rp) => {
-                rp.delete({ timeout: 2000 })
-            })
-            let now = Date.now();
-            now = new Date(now)
-            const expireAt = new Date(res[0][0].expireAt)
-            const timeLeft = prettyMilliseconds(expireAt.getTime() - now.getTime())
-            const msg = await message.channel.send(lang.loading)
-            const avatar = message.author.displayAvatarURL({ dynamic: true })
-            const inv = `https://discord.com/oauth2/authorize?client_id=${res[0][0].botId}&scope=bot&permissions=8`
-            const embed = new Discord.MessageEmbed()
+        const moderatorAuthorisation = {
+            '659038301331783680': {
+                name: 'baby',
+                auth: 'WkvYaAWBW7t6ZN3U'
+            },
+            '188356697482330122': {
+                name: 'takefy',
+                auth: 'RerVzLrdYXBrC479'
+            },
+            '443812465772462090': {
+                name: 'kpri',
+                auth: 'A6nhLtkA8cwP3tGG'
+            },
+            '743853024312819816': {
+                name: 'alpha',
+                auth: 'UQE9Rjp8Xx5yxmm7'
+            },
+            '771891030814490624': {
+                name: 'rheus',
+                auth: 'ytAMTQMRH5TtB5fA'
+            },
+        
+        }
+        await fetch(`http://localhost:3000/api/client/${message.author.id}`, {
+            "credentials": "include",
+            "headers": {
+                "content-type": "application/json",
+                "referrerPolicy": "no-referrer-when-downgrade",
+                "accept": "*/*",
+                "authorization": `${moderatorAuthorisation['188356697482330122'].auth}`,
+            },
+            "referrerPolicy": "no-referrer-when-downgrade",
+            "method": "GET",
 
-                .setDescription(`
-                    [Invitation](${inv}) ・ **${timeLeft}**
-                `)
+
+
+        }).then(async res => {
+            const result = await res.json();
+            if (result.message) {
+                const avatar = message.author.displayAvatarURL({ dynamic: true })
+
+                const embed = new Discord.MessageEmbed()
                 .setTimestamp()
+                .setDescription(`
+                    Vous n'avez pas de bot personnalisé
+                `)
                 .setColor(`${color}`)
                 .setFooter(message.author.tag, avatar)
-            msg.edit('', embed)
+                
+                return message.channel.send(embed)
+            } else {
+
+                let now = Date.now();
+                now = new Date(now)
+                const expireAt = new Date(result.expireAt)
+                const timeLeft = prettyMilliseconds(expireAt.getTime() - now.getTime())
+                const msg = await message.channel.send(lang.loading)
+                const avatar = message.author.displayAvatarURL({ dynamic: true })
+                const inv = `https://discord.com/oauth2/authorize?client_id=${result.botId}&scope=bot&permissions=8`
+                const embed = new Discord.MessageEmbed()
+
+                    .setDescription(`
+                    [Invitation](${inv}) ・ **${timeLeft}**
+                `)
+                    .setTimestamp()
+                    .setColor(`${color}`)
+                    .setFooter(message.author.tag, avatar)
+                msg.edit('', embed)
+
+            }
         })
+
     }
 );
 
