ccf8503b87302578f89deb8e9e4061651d988f9b
takefy-dev <jules001dixon@gmail.com>
2021-03-19 17:52:41 +0100

    finish desing now playing

--- commands/music/playing.js
+++ commands/music/playing.js
@@ -21,9 +21,7 @@ module.exports = new Command({
     const lang = require(`../../lang/${guildLang.get(message.guild.id)}`);
     const queue = client.music.getQueue(message)
     if (!queue) return message.channel.send(lang.music.nothingInQueue)
-    console.log(queue)
     const song = queue.songs[0];
-    console.log(song)
     let minutes = song.formattedDuration.split(":")[0];   
     let seconds = song.formattedDuration.split(":")[1];    
     let ms = (Number(minutes)*60+Number(seconds));   
@@ -32,10 +30,10 @@ module.exports = new Command({
     const seek = (queue.connection.dispatcher.streamTime - queue.connection.dispatcher.pausedTime) / 1000;
    // define left duration
     const left = ms - seek;
-    const likes = song.likes;
-    const dislikes = song.dislikes;
-    const reposts = song.reposts;
-    const view = song.views;
+    const likes = song.likes.toLocaleString();
+    const dislikes = song.dislikes.toLocaleString();
+    const reposts = song.reposts.toLocaleString();
+    const view = song.views.toLocaleString();
     const name = song.name;
     const url = song.url;
     const thumbnail = song.thumbnail;
@@ -44,8 +42,8 @@ module.exports = new Command({
     const category = song.info.videoDetails.category;
     let nowPlaying = new Discord.MessageEmbed()
           .setAuthor('Now playing ♪','https://cdn.discordapp.com/attachments/820031925389230110/822494236007596082/3dgifmaker81211.gif','https://www.one4all.fr')
-          .setDescription(`<:title:783422216095793172> Title: [**${song.name}**](${song.url})\n<:asked:822506067639926804> Requested by: **${requestedBy}**\n:eyes: Views: **${song.views}**\n<:like:822504740519673856> Likes: **${song.likes}**\n<:dislike:822504631967678494> Dislikes: **${song.dislikes}**\n<:cateogry:822505206801498132> Category: **${category}**\n<:rate:822505460447051796> Average Rate: **${averageRate}**\n<a:reupload:822505791759712266> Reposts: **${reposts}**`)
-          .setThumbnail(song.thumbnail)
+          .setDescription(`<:title:783422216095793172> Title: [**${name}**](${url})\n<:asked:822506067639926804> Requested by: **${requestedBy}**\n:eyes: Views: **${view}**\n<:like:822504740519673856> Likes: **${likes}**\n<:dislike:822504631967678494> Dislikes: **${dislikes}**\n<:cateogry:822505206801498132> Category: **${category}**\n<:rate:822505460447051796> Average Rate: **${averageRate}**\n<a:reupload:822505791759712266> Reposts: **${reposts}**`)
+          .setThumbnail(thumbnail)
           .setColor(`${color}`)
           .setFooter(`${lang.music.requestedBy} ${requestedBy.username}#${requestedBy.discriminator}`, requestedBy.displayAvatarURL({ dynamic: true }))
           .addField("\u200b", "\u200b");
@@ -57,7 +55,7 @@ module.exports = new Command({
       }
       //If its not a stream 
       if (ms > 0 && ms<10000) {
-        nowPlaying.addField("<:time:783422233988562964> **TIME:**", "`[" + createBar((ms == 0 ? seek : ms), seek, 25, "▬", "🟢")[0] + "]`\n" + "\n[" + new Date(seek * 1000).toISOString().substr(11, 8) + " / " + (ms == 0 ? " ◉ LIVE" : new Date(ms * 1000).toISOString().substr(11, 8))+ "]" + "\n" + `\n ${lang.music.currentPlaying.timeLeft}` + "`" + new Date(left * 1000).toISOString().substr(11, 8) + "`", false ); // the bar
+        nowPlaying.addField("<:time:783422233988562964> **TIME:**", "`[" + createBar((ms == 0 ? seek : ms), seek, 25, "▬", "🟢")[0] + "]`\n" + "`" + "[" + new Date(seek * 1000).toISOString().substr(11, 8) + " / " + (ms == 0 ? " ◉ LIVE" : new Date(ms * 1000).toISOString().substr(11, 8))+ "]" + "`" + "\n" + "`" + new Date(left * 1000).toISOString().substr(11, 8) + "`", false ); // the bar
         //send approve msg
         return message.channel.send(nowPlaying);
       }
