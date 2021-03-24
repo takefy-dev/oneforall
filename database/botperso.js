c7999982f4380414b36382885ff074fa70bd0d3f
takefy-dev <jules001dixon@gmail.com>
2021-03-24 19:07:22 +0100

    fix sql error

--- database/botperso.js
+++ database/botperso.js
@@ -2,12 +2,25 @@ require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 
+const fs = require('fs');
+const path = './config.json';
+let user;
+let name;
+let pass;
+if (fs.existsSync(path)) {
+    const config = require('../config.json')
+   
+        user = config.dbuser
+        pass= config.dbPass
 
-module.exports = mysql.createConnection({ 
-    user     : process.env.DB_USER,
-    password : process.env.DB_PASS,
+} else {
+    user =process.env.DB_USER
+    pass=process.env.DB_PASS
+}
+
+
+
+module.exports = mysql.createConnection({
+    user: user,
+    password: pass,
 });
-
-
-    
-
