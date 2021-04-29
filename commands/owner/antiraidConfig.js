817f3b168b93e699566201daa4ce860c23236db5
takefy-dev <jules001dixon@gmail.com>
2021-04-29 10:46:21 +0200

    put import antiraid config

--- commands/owner/antiraidConfig.js
+++ commands/owner/antiraidConfig.js
@@ -2,7 +2,10 @@
 const Command = require('../../structures/Handler/Command');
 const {Logger} = require('advanced-command-handler')
 const NumberFromEmoji = require('../../utils/emojiToNumber')
-
+const emojiEnable = {
+    true: "<:778348494712340561:781153837850820619>",
+    false: "<:778348495157329930:781189773645578311>"
+}
 module.exports = class Test extends Command {
     constructor() {
         super({
