7def481202ccf3231020007fbd5bd728d6924f69
takefy-dev <jules001dixon@gmail.com>
2021-04-29 21:26:48 +0200

    addemoji now with links

--- commands/misc/addemoji.js
+++ commands/misc/addemoji.js
@@ -27,27 +27,28 @@ module.exports = class Test extends Command {
         const emoji = args[0];
         let custom = Util.parseEmoji(emoji);
 
-
         if (!emoji) {
             return message.channel.send(lang.addemoji.missingUrl);
         }
-
-        const name = args[1] ? args[1].replace(/[^a-z0-9]/gi, "") : null;
-        if (!name) {
+        let name = args[1] ? args[1].replace(/[^a-z0-9]/gi, "") : null;
+        if (!custom.id && !name) {
             return message.channel.send(lang.addemoji.missingName);
         }
-        if (name.length < 2 || name > 32) {
+        if (name && (name.length < 2 || name > 32)) {
             return message.channel.send(lang.addemoji.invalidName);
         }
+        if(!name) name = custom.id
+
+        let link = args[0]
         if (custom.id) {
-            let link = `https://cdn.discordapp.com/emojis/${custom.id}.${custom.animated ? "gif" : "png"}`
-            message.guild.emojis.create(link, name, {reason: `emoji add par ${message.author.username}`}).then(() => {
-                message.channel.send(lang.addemoji.success(name))
-                Logger.log(`${Logger.setColor('teal')}${message.author.tag} a ajouté un emoji`, 'Success add emoji')
-            })
+            link = `https://cdn.discordapp.com/emojis/${custom.id}.${custom.animated ? "gif" : "png"}`
+
 
         }
-
+        message.guild.emojis.create(link, name, {reason: `emoji add par ${message.author.username}`}).then(() => {
+            message.channel.send(lang.addemoji.success(name))
+            Logger.log(`${Logger.setColor('teal')}${message.author.tag} a ajouté un emoji`, 'Success add emoji')
+        })
     }
 }
 
