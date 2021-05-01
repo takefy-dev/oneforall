f919ab8356c4f9b90bea7413e77ce1da629ac336
takefy-dev <jules001dixon@gmail.com>
2021-05-01 13:24:47 +0200

    soutien adapt new db

--- events/soutien/presenceUpdate.js
+++ events/soutien/presenceUpdate.js
@@ -1,76 +1,53 @@
 const StateManager = require('../../utils/StateManager');
 const Discord = require('discord.js')
-const soutienId = new Map();
-const soutienMsg = new Map();
-const soutienisOn = new Map();
+
 
 const Event = require('../../structures/Handler/Event');
 
 module.exports = class presenceUpdate extends Event {
-  constructor() {
-    super({
-      name: 'presenceUpdate',
-    });
-  }
+    constructor() {
+        super({
+            name: 'presenceUpdate',
+        });
+    }
 
-  async run(client, oldMember, newMember) {
-    // console.log(newMember)
-    this.connection = StateManager.connection;
-    if (!client
-.BotPerso) return;
-    client
-.guilds.cache.forEach(guild => {
+    async run(client, oldMember, newMember) {
+        this.connection = StateManager.connection;
+        if (!client.BotPerso) return;
+        client.guilds.cache.forEach(guild => {
 
-      if (!oldMember) return;
-      const msg = soutienMsg.get(guild.id);
-      const roleId = soutienId.get(guild.id);
-      const isOn = soutienisOn.get(guild.id);
-      let status = newMember.user.presence.activities.map(a => a.state)
-      if (guild.members.cache.get(newMember.user.id) == undefined) return;
+            if (!oldMember) return;
+            const msg = guild.config.soutienMsg
+            const roleId = guild.config.soutienId
+            const role = guild.roles.cache.get(roleId)
+            if (!role) return;
+            const isOn = guild.config.soutienOn;
+            let status = newMember.user.presence.activities.map(a => a.state)
+            if (guild.members.cache.get(newMember.user.id) === undefined) return;
 
-      if (isOn == "0") {
-        return;
+            if (isOn) {
+                return;
 
-      } else if (oldMember == undefined || newMember == undefined) {
-        return;
-      } else if (oldMember.status != newMember.status || oldMember == undefined || newMember == undefined) {
-        return;
-      } else if (isOn == "1") {
-        if (status[0] != null && status[0].includes(msg)) {
-          guild.members.cache.get(newMember.user.id).roles.add(roleId)
-        } else {
-          if (guild.members.cache.get(newMember.user.id) == undefined) return;
-          if (guild.members.cache.get(newMember.user.id).roles.cache.some((r) => r.id === roleId)) {
-            guild.members.cache.get(newMember.user.id).roles.remove(roleId)
-          }
-        }
-      }
-    });
-  }
+            } else if (oldMember === undefined || newMember === undefined) {
+                return;
+            } else if (oldMember.status !== newMember.status || oldMember == undefined || newMember == undefined) {
+                return;
+            } else if (isOn ) {
+                if (status[0] != null && status[0].includes(msg)) {
+                    guild.members.cache.get(newMember.user.id).roles.add(roleId)
+                } else {
+                    if (guild.members.cache.get(newMember.user.id) === undefined) return;
+                    if (guild.members.cache.get(newMember.user.id).roles.cache.some((r) => r.id === roleId)) {
+                        guild.members.cache.get(newMember.user.id).roles.remove(roleId)
+                    }
+                }
+            }
+        });
+    }
 }
 
-StateManager.on('soutienIdFetched', (guildId, soutienIds) => {
-  soutienId.set(guildId, soutienIds);
-})
 
-StateManager.on('soutienIdUpdate', (guildId, soutienIds) => {
-  soutienId.set(guildId, soutienIds);
-})
 
-StateManager.on('soutienMsgFetched', (guildId, soutienMsgs) => {
-  soutienMsg.set(guildId, soutienMsgs);
-})
-
-StateManager.on('soutienMsgUpdate', (guildId, soutienMsgs) => {
-  soutienMsg.set(guildId, soutienMsgs);
-})
-StateManager.on('soutienOn', (guildId, soutienMsgs) => {
-  soutienisOn.set(guildId, soutienMsgs);
-})
-
-StateManager.on('soutienOnFetched', (guildId, soutienOn) => {
-  soutienisOn.set(guildId, soutienOn);
-})
 
 
 
