625c9a2f2e602d10e952768991a75c6e9d840fe8
takefy-dev <jules001dixon@gmail.com>
2021-05-03 11:26:34 +0200

    update for peros

--- structures/Client/OneForAll.js
+++ structures/Client/OneForAll.js
@@ -1,20 +1,43 @@
-const { Collection, Client } = require('discord.js');
-const { Sequelize } = require('sequelize');
+const {Collection, Client} = require('discord.js');
+const {Sequelize} = require('sequelize');
 const config = require('../../config')
 const logs = require('discord-logs')
 const fs = require('fs')
-const { Logger } = require('advanced-command-handler')
+const {Logger} = require('advanced-command-handler')
 const Distube = require('distube');
-module.exports = class OneForAll extends Client{
+let user =  config.database.user;
+let name = config.database.name;
+let pass = config.database.password;
+let token = config.token;
+let owner = [...config.owners];
+if(config.botperso){
+    const path = './config.json';
+    if (fs.existsSync(path)) {
+        const configs = require('../../config.json')
+        owner.push(configs.owner);
+        user = configs.dbuser;
+        name = configs.dbName;
+        pass = configs.dbPass
+        token = configs.token
+    } else {
+        owner.push(process.env.OWNER)
+        user = process.env.DB_USER
+        token = process.env.TOKEN
+        pass = process.env.DB_PASS
+        name = process.env.DB_NAME
+    }
+}
+
+module.exports = class OneForAll extends Client {
     constructor(options) {
         super(options);
         this.commands = new Collection();
         this.events = new Collection();
         this.aliases = new Collection();
         this.config = config;
-        this.owners = config.owners;
+        this.owners = owner;
         this.cooldown = new Collection();
-        this.database = new Sequelize(config.database.name, config.database.user, config.database.password,{
+        this.database = new Sequelize(name, user,pass, {
             dialect: 'mysql',
             define: {
                 charset: "utf8",
@@ -38,28 +61,29 @@ module.exports = class OneForAll extends Client{
         this.initDatabase()
         this.botperso = config.botperso;
         this.maintenance = false;
-        this.music = new Distube(this, { searchSongs: false, leaveOnEmpty: true});
+        this.music = new Distube(this, {searchSongs: false, leaveOnEmpty: true});
 
     }
-    lang(translate){
+
+    lang(translate) {
         return require(`../../lang/${translate}`);
     }
 
 
-    loadCommands(){
-        const commmandsFolder = fs.readdirSync('./commands/').filter(name =>name !== 'stats');
+    loadCommands() {
+        const commmandsFolder = fs.readdirSync('./commands/').filter(name => name !== 'stats');
         Logger.info(`Loading ${commmandsFolder.length} category`, `Starting`)
 
-        for (const category of commmandsFolder){
+        for (const category of commmandsFolder) {
 
             const commandFiles = fs.readdirSync(`./commands/${category}/`).filter(file => file.endsWith('.js'));
             Logger.comment(`${Logger.setColor('red', `Loading: ${commandFiles.length} commands in ${category}`)}`, `Loading commands`)
 
-            for (const file of commandFiles){
+            for (const file of commandFiles) {
                 const CommandFile = require(`../../commands/${category}/${file}`)
                 const Command = new CommandFile();
                 this.commands.set(Command.name, Command);
-                for(const alias of Command.aliases){
+                for (const alias of Command.aliases) {
                     this.aliases.set(alias, Command)
                 }
 
@@ -67,14 +91,15 @@ module.exports = class OneForAll extends Client{
             }
         }
     }
-    loadEvents(){
+
+    loadEvents() {
         const eventsFolder = fs.readdirSync('./events/')
         Logger.info(`Loading ${eventsFolder.length} events category`, `Starting`)
-        for(const category of eventsFolder){
+        for (const category of eventsFolder) {
             const eventsFile = fs.readdirSync(`./events/${category}/`).filter(file => file.endsWith('.js'))
             Logger.comment(`${Logger.setColor('red', `Loading: ${eventsFile.length} events in ${category}`)}`, `Loading events`)
 
-            for (const event of eventsFile){
+            for (const event of eventsFile) {
 
                 const EventFile = require(`../../events/${category}/${event}`);
                 const Event = new EventFile(this);
@@ -83,14 +108,14 @@ module.exports = class OneForAll extends Client{
 
                 Logger.comment(`${Logger.setColor('green', `Bind: ${event.split('.js')[0]}`)}`, `Binding events`)
             }
-       }
+        }
 
     }
 
-    initDatabase(){
+    initDatabase() {
         this.database.authenticate().then(async () => {
             const modelsFile = fs.readdirSync('./structures/models');
-            for await (const model of modelsFile){
+            for await (const model of modelsFile) {
                 await require(`../models/${model}`)(Sequelize, this)
                 Logger.log(`${Logger.setColor(`red`, `${model.split('.')[0]}`)}`, 'LOADED DATABASE')
             }
@@ -106,7 +131,7 @@ module.exports = class OneForAll extends Client{
         })
     }
 
-    isOwner(checkId){
+    isOwner(checkId) {
         return !!this.owners.includes(checkId)
     }
 }
