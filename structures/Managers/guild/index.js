8d1833fbbfcd74acb53734ecb06e7c145f6045c7
baby <63604401+whoisbaby@users.noreply.github.com>
2021-07-23 12:31:33 +0300

    Update index.js

--- structures/Managers/guild/index.js
+++ structures/Managers/guild/index.js
@@ -172,8 +172,8 @@ class GuildManager {
                 commands: {
                     perm1: ['clear', 'warn', 'unwarn', 'warnlist', 'warnsettings', 'mute', 'mutelist', 'tempmute', 'unmute', 'addemoji', 'rmemoji'],
                     perm2: ['ban', 'kick', 'lockchannel', 'unban', 'clear', 'alladmin', 'allbot', 'banlist'],
-                    perm3: ['derank', 'nuke', 'say', 'soutien', 'gcreate', 'greroll'],
-                    perm4: ['addinvite', 'clearinvite', 'removeinvite', 'compteur', 'embedBuilder', 'massiverole', 'reactionRoleMenu', 'role', 'tempvoc', 'setcolor', 'webhook']
+                    perm3: ['derank', 'nuke', 'say', 'soutien', 'gcreate', 'greroll', 'voicekick', 'glist', 'gend'],
+                    perm4: ['cleanup', 'piconly', 'addinvite', 'clearinvite', 'removeinvite', 'compteur', 'embedBuilder', 'massiverole', 'reactionRoleMenu', 'role', 'tempvoc', 'setcolor', 'webhook']
                 },
                 enable: false
             },
