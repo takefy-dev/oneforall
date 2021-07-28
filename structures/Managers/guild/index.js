c212b0363f0ab7edfc93396c8f4a14deb9f3a19f
takefy-dev <jules001dixon@gmail.com>
2021-07-28 22:37:02 +0200

    edit default config of xp

--- structures/Managers/guild/index.js
+++ structures/Managers/guild/index.js
@@ -113,7 +113,7 @@ class GuildManager {
                 role: undefined
             }],
             reactionsToMessages: values.reactionsToMessages ? values.reactionsToMessages : [],
-            xp : values.xp ? values.xp : {xpPerMsg:'1-2', xpPerSVoc: 2, enable: true, allowChannels: ['all'], forbidChannels: [], multiplerChannels: []}
+            xp : values.xp ? values.xp : {xpPerMsg:'1-30', xpPerSVoc: "1-20", enable: false, allowChannels: ['all'], forbidChannels: [], multiplerChannels: []}
         }
         this.cachedInv = new Collection()
         this.snipes = new Collection()
